063468163ef3a37fa98fb58c491ba12ba8375e8b
Gaurav <gauravagerwala@gmail.com>
2016-02-06 10:48:10 +0530

    Got city, lat from the IP address

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var weather = require('./weather.js');
+var location = require('./location');
 
 weather(function(currentWeather){
   console.log(currentWeather);
 });
+
+location(function(location){
+  console.log("City is: "+ location.city);
+  console.log("long/lat is :" + location.loc);
+})
---
+++ location.js
@@ -0,0 +1,15 @@
+var url = 'http://ipinfo.io'
+var request = require('request');
+
+module.exports = function (callback){
+  request({
+    url: url,
+    json: true
+  }, function (error, response, body){
+    if(error){
+      callback();
+    }else{
+      callback(body);
+    }
+  });
+};
